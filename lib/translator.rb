f68416a0c715067f8549c7c312160c35477448e5
Connor Meth <connormeth27@gmail.com>
2019-09-14 17:50:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,8 +24,8 @@ end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   lib = load_library(file_path)
-  if lib["get_emoticon"].include?(japanese_emoticon)
-    lib["get_emoticon"][japanese_emoticon]
+  if lib["get_meaning"].include?(japanese_emoticon)
+    lib["get_meaning"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
